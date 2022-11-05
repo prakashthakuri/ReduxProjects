637d8c0e5773fa58a174ea28bc0245ca3944ad78
Prakash Shahi Thakuri <prakashthakuri1994@gmail.com>
2022-11-05 16:21:38 -0500

    payload with objects

--- redux-payload-example/src/components/LanguageAuthor.js
+++ redux-payload-example/src/components/LanguageAuthor.js
@@ -1,21 +1,28 @@
 import React, {useState} from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function LanguageAuthor() {
 
+
     const [languageName, setLanguageName] = useState('')
 
+    const languageObj = useSelector(state => state)
     const dispatch = useDispatch()
 
     const changeLanguage = () => {
+        // changing payload to object.
+        const newPayload = {
+            languageName: languageName,
+            languagePopularFramework: languageName === 'Java' ? 'Sprint Boot' : 'React'
+        }
 
         // Dispatch takes action type and payload, type is what we define and payload is the new object we want update
-        dispatch({type: 'CHANGE_NAME', payload: languageName})
+        dispatch({type: 'CHANGE_NAME', payload: newPayload})
     } 
 
     return (
         <>
-        <h1> Most Common Framework is:</h1>
+        <h1> Most Common Framework is: {languageObj.languagePopularFramework}</h1>
         <input 
         type="text"
         placeholder= "new language"
--- redux-payload-example/src/reducers/languageReducers.js
+++ redux-payload-example/src/reducers/languageReducers.js
@@ -1,16 +1,22 @@
 
 // This is the store
 const initialData = {
-    languageName : 'JavaScript'
+    languageName : 'JavaScript',
+    languagePopularFramework: 'React'
 }
 
 // store is defined as a state in reducer function
+
+// reducers has two parameter 1. state and 2. action (props: actionName and payload), actionName: action.type
 export default function LanguageReducers(state = initialData, action) {
     
     // eslint-disable-next-line default-case
     switch (action.type) {
         case 'CHANGE_NAME' : return { 
-            languageName: action.payload}
+            ...state,
+            languageName: action.payload.languageName, // if the payload is object you need to assign the objectName too
+            languagePopularFramework: action.payload.languagePopularFramework
+        }
     }
 
     return state
